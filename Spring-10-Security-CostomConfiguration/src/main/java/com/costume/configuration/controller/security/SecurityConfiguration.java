6204e4c6103246413104c3806a4ec7980f80b66d
Ruslan Kasymov <russkasymov@gmail.com>
2021-01-13 21:32:11 -0500

    Implement Default Configuration and Costume Configuration.

---
+++ Spring-10-Security-CostomConfiguration/src/main/java/com/costume/configuration/controller/security/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.costume.configuration.controller.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()                    // request should be authorized
+                .anyRequest().authenticated()          // incoming request be authenticated
+                .and()
+                .httpBasic();                           //perform basic authentication
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .and()
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager"))
+                .roles("MANAGER");
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
