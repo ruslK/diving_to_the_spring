5f18ba8e2adb34aa7de9e1367e09b4ad02632a21
Ruslan Kasymov <russkasymov@gmail.com>
2021-01-16 14:52:06 -0500

    Implement HTTP Basic Auth Config with DB

---
+++ Spring-10-Security-HttpBasic-DataBase/src/main/java/com/securitydb/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.securitydb.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()                    // request should be authorized
+                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**", "/management/**", "/admin/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("MANAGER", "ADMIN")
+                .and()
+                .httpBasic();                           //perform basic authentication
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ Spring-10-Security-HttpBasic-DataBase/src/main/java/com/securitydb/security/UserPrincipalDetailsService.java
@@ -0,0 +1,25 @@
+package com.securitydb.security;
+
+import com.securitydb.entity.User;
+import com.securitydb.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        UserPrinciple userPrinciple = new UserPrinciple(user);
+        return userPrinciple;
+    }
+}
